0c7cc5a5627a2c2bd3d5f27a3da5f6b7a5d7fcd0
Derek Ruize <derekruiz@usf.edu>
2025-02-16 21:32:17 -0500

    Task 4 - Car Rental Cost Calculation

--- cc_8.js
+++ cc_8.js
@@ -25,10 +25,28 @@ const calculateServiceFee = (amount,serviceType) => {
         serviceFee = 0.1
     } else { if (serviceType === "Basic") {
         serviceFee = 0.05
-    }}}
+    }}} //Different fees for each tier
 
     console.log("Service Fee:", "$" + amount * serviceFee);
 }
 
 calculateServiceFee(200, "Premium"); 
 calculateServiceFee(500, "Standard"); 
+
+//Task 4 - Parameters and Arguments 
+function calculateRentalCost(days, carType, insurance = false) {
+    const carRate = {
+        "Economy": 40,
+        "Standard": 60,
+        "Luxury": 100
+    } //Rates for each car type
+
+    let insuranceFee = 0
+    if (insurance === true) {
+        insuranceFee = 20 
+    } //Insurance rate if chosen 
+    console.log("Total Rental Cost:", "$" + ((carRate[carType] * days) + (insuranceFee * days))) //Formula to calculate cost
+}
+
+calculateRentalCost(3, "Economy", true); 
+calculateRentalCost(5, "Luxury", false); 
